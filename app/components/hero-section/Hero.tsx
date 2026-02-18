ad6384a607ccae988492c058141e965d772383af
DATTATRAY KUMAR WAGHMARE <165990480+Waghmaredatta713@users.noreply.github.com>
2026-02-18 14:53:30 +0530

    Update Hero.tsx

--- app/components/hero-section/Hero.tsx
+++ app/components/hero-section/Hero.tsx
@@ -95,9 +95,9 @@ export default function Hero() {
         >
           <p className="text-white/60 inline">I&apos;m </p>
           <span className="bg-gradient-to-br bg-clip-text text-transparent from-[#7CC0C4] via-[#548FBA] to-[#3C84C7]">
-          your_username
+          Dattatray Waghmare
           </span>
-          <p>a DevOps Engineer</p>
+          <p>a IT Infrastructure Engineer</p>
         </motion.h1>
 
         <motion.p
